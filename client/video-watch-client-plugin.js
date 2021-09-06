8fa4f61876200903e8702c37926eaf4c2b24d811
Martin Darmüntzel <martin@trivialanalog.de>
2021-09-06 06:13:22 +0200

    Versteckt den Downloadknopf.

--- client/video-watch-client-plugin.js
+++ client/video-watch-client-plugin.js
@@ -1,5 +1,11 @@
 function register ({ registerHook, peertubeHelpers }) {
-
+	registerHook({
+		target: 'action:video-watch.player.loaded',
+		handler: () => {
+			// disable download
+			document.getElementById('download-direct').parentElement.style.display = 'none'
+		}
+	})
 }
 
 export {
