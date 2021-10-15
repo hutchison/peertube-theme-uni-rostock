afe46da42ab8bda7b7d902cd9bad0d62835ac78d
Martin Darmüntzel <martin@trivialanalog.de>
2021-10-15 09:03:41 +0200

    Nächster Versuch den Download zu unterbinden.

--- client/video-watch-client-plugin.js
+++ client/video-watch-client-plugin.js
@@ -3,8 +3,14 @@ function register ({ registerHook, peertubeHelpers }) {
 		target: 'action:video-watch.player.loaded',
 		handler: () => {
 			// disable download
-			document.querySelector('my-global-icon.icon-download').parentElement.style.display = 'none';
-			document.querySelector("my-global-icon[iconname='download']").parentElement.style.display = 'none';
+			var download_icon = document.querySelector('my-global-icon.icon-download');
+			if (download_icon != null) {
+				download_icon.parentElement.style.display = 'none';
+			}
+			download_icon = document.querySelector("my-global-icon[iconname='download']");
+			if (download_icon != null) {
+				download_icon.parentElement.style.display = 'none';
+			}
 		}
 	})
 }
